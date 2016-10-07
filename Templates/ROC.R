87a3d0c803f9ad17e0569c2e25bc284ab23cb2f4
Gurupradeep <gurupradeept@gmail.com>
2016-10-07 16:56:37 +0530

    Comments added

--- Templates/ROC.R
+++ Templates/ROC.R
@@ -2,6 +2,8 @@ install.packages("ROCR")
 library(ROCR)
 data(ROCR.simple)
 print(ROCR.simple$labels)
+
+#$Predictions has continous values between O and 1 which are predicted by our model and $labels are the actual output values which should be binary.
 pred <- prediction( ROCR.simple$predictions, ROCR.simple$labels)
 perf <- performance(pred,"tpr","fpr")
 plot(perf)
